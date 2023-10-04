8bd4e00cb2a1ec8bd14284c7221effeb63c4377a
Saro Babikian <122392083+saro9588@users.noreply.github.com>
2023-10-03 22:30:53 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ function Home(){
     return (
       <Card
         txtcolor="black"
-        header="BadBank Landing Module"
-        title="Welcome to the bank"
-        text="You can move around using the navigation bar."
+        header="MIT Bank Landing Module"
+        title="Welcome to the Bank App"
+        text="You can create an account then deposit/withdraw money!"
         body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
       />
     );  
